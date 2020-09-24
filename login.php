83db0b0193e4ea13b2b181f5f90513debe2269b4
Prevail03 <prevamu03gmail.com>
2020-09-25 01:39:02 +0300

    This is My first Commit!

---
+++ login.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>KHF/Admin</title>
+
+<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="home.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+	<style type="text/css">body{background-color: #2f3542;} img{border-radius: 50%;}p{color: white;}</style>
+<div class="heading" ><img src="images\logo\khf1.jpg" alt="images/khflogo" height="200" width="200">Kenya Handball Federation</div>
+<p>Welcome</p>
+
+ <a href='insert.php' class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Enter New Results</a>
+ <a href='view.php' class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Results</a>
+ <a href='upload.php' class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Upload Player Profile</a>
+ <a href='profileAdmin.php' class="btn btn-primary btn-lg active" role="button" aria-pressed="true">View Profiles</a>
+ <a href='leagueAdmin.php' class="btn btn-primary btn-lg active" role="button" aria-pressed="true">League Standings</a>
+ <a href='update.php' class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Update  News</a>
+ <a href='TeamDetails.php' class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Add New Team</a>
+ <a href='logout.php' class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Logout</a><br><br>
+<br /><br /><br /><br />
+
+</div>
+<footer class="footer-distributed">
+
+      <div class="footer-left">
+
+        
+        <div style="border-radius: 50%;"> 
+        <img src="images\logo\khf1.jpg" alt="images/khflogo" height="200" width="200">
+        </div>
+        <p class="footer-links">
+          <a href="about.php">About</a>
+          
+          <a href="contact.php">Contact</a>
+        </p>
+
+        <p class="footer-company-name">Kenya Handball Federation &#169; 2020</p>
+      </div>
+
+      <div class="footer-center">
+
+        <div>
+          <img src="imForSite/location.png" alt="location icon"  height="20" width="20">
+          <p>Lang`ata Rd, Nyayo Stadium Handball Court </p>
+        </div>
+
+        <div>
+          <img src="imForSite/call.png" alt="smartphone logo"  height="20" width="20">
+          <p>+254 701694441</p>
+        </div>
+
+        <div>
+          <img src="imForSite/email.jpg" alt="gmail logo" height="20" width="20" >
+          <p><a href="mailto:khfkenya1@gmail.com">khfkenya1@gmail.com.com</a></p>
+        </div>
+
+      </div>
+
+      <div class="footer-right">
+
+        <p class="footer-company-about">
+          <span>Competitions we Are Associated With:</span>
+            
+              <ul type="Square">
+                <li>Kenya Universities Sports Assocciation</li>
+                <li>Kenya Handball Federation Handball National League </li>
+                <li>Kenya Secondary Schools Sports Association</li>
+              </ul>
+            
+        </p>
+
+        <div class="footer-icons">
+
+          <a href="https://web.facebook.com/kenyahandballfederation" target="_blank"><img src="imForSite/facebook.png" alt="facebook logo" target="_blank" height="20" width="20"></a>
+          <a href="https://twitter.com/handball_kenya" target="_blank"><img src="imForSite/twitter-logo.png" alt="twitter logo"  height="20" width="20"></a>
+          <a href="https://api.whatsapp.com/send?phone=254701694441" target="_blank"><img src="imForSite/whatsapp-logo.png" alt="whatsapp" height="20" width="20"></a>
+          <a href="https://www.instagram.com/kenyahandball/" target="_blank"><img src="imForSite/instagram.png" alt="instagram logo" height="20" width="20" ></a>
+          <a href="mailto: khfkenya1@gmail.com" target="_blank"><img src="imForSite/email.jpg" alt="gmail logo" height="20" width="20" ></a>
+        </div>
+
+      </div>
+
+    </footer>
+</body>
+</html>
